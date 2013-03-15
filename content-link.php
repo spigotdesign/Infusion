11bc642f907e77b5611eb0044dc89dca6ef36f95
Bryan Hoffman <bryan@spigotdesign.com>
2013-03-15 15:39:16 -0600

    content classes added to content-link.php

--- content-link.php
+++ content-link.php
@@ -2,17 +2,17 @@
 
 	<?php if ( is_singular( get_post_type() ) ) { ?>
 
-		<header class="entry-header">
-			<h1 class="entry-title"><?php single_post_title(); ?></h1>
+		<header class="entry-header link-header">
+			<h1 class="entry-title link-title"><?php single_post_title(); ?></h1>
 			<?php echo apply_atomic_shortcode( 'entry_byline', '<div class="entry-byline">' . __( '[post-format-link] published on [entry-published] [entry-comments-link before=" | "] [entry-edit-link before=" | "]', 'hybrid-base' ) . '</div>' ); ?>
-		</header><!-- .entry-header -->
+		</header><!-- .entry-header link-header -->
 
-		<div class="entry-content">
+		<div class="entry-content link-content">
 			<?php the_content(); ?>
 			<?php wp_link_pages( array( 'before' => '<p class="page-links">' . '<span class="before">' . __( 'Pages:', 'hybrid-base' ) . '</span>', 'after' => '</p>' ) ); ?>
 		</div><!-- .entry-content -->
 
-		<footer class="entry-footer">
+		<footer class="entry-footer link-footer">
 			<?php echo apply_atomic_shortcode( 'entry_meta', '<div class="entry-meta">' . __( '[entry-terms taxonomy="category" before="Posted in "] [entry-terms before="Tagged "]', 'hybrid-base' ) . '</div>' ); ?>
 		</footer><!-- .entry-footer -->
 
@@ -20,20 +20,20 @@
 
 		<?php if ( get_the_title() ) { ?>
 
-			<header class="entry-header">
-				<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( post_format_tools_url_grabber() ) . '" title="' . the_title_attribute( array( 'echo' => false ) ) . '">', '<span class="meta-nav">&rarr;</span></a></h2>' ); ?>
-			</header><!-- .entry-header -->
+			<header class="entry-header link-header">
+				<?php the_title( '<h2 class="entry-title link-title"><a href="' . esc_url( post_format_tools_url_grabber() ) . '" title="' . the_title_attribute( array( 'echo' => false ) ) . '">', '<span class="meta-nav">&rarr;</span></a></h2>' ); ?>
+			</header><!-- .entry-header link-header -->
 
 		<?php } else { ?>
 
-			<div class="entry-content">
+			<div class="entry-content link-content">
 				<?php the_content( __( 'Read more &rarr;', 'hybrid-base' ) ); ?>
 				<?php wp_link_pages( array( 'before' => '<p class="page-links">' . '<span class="before">' . __( 'Pages:', 'hybrid-base' ) . '</span>', 'after' => '</p>' ) ); ?>
 			</div><!-- .entry-content -->
 
 		<?php } ?>
 
-		<footer class="entry-footer">
+		<footer class="entry-footer link-footer">
 			<?php echo apply_atomic_shortcode( 'entry_meta', '<div class="entry-meta">' . __( '[post-format-link] published on [entry-published] [entry-permalink before="| "] [entry-comments-link before="| "] [entry-edit-link before="| "]', 'hybrid-base' ) . '</div>' ); ?>
 		</footer><!-- .entry-footer -->
 
