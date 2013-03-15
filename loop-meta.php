d860c125ee0cef1868d8e9091817f591de1dc869
Bryan Hoffman <bryan@spigotdesign.com>
2013-03-15 15:39:40 -0600

    Descriptive classes added to loop-meta.php

--- loop-meta.php
+++ loop-meta.php
@@ -8,49 +8,49 @@ if ( is_singular() )
 
 	<?php if ( is_home() && !is_front_page() ) { ?>
 
-		<h1 class="loop-title"><?php echo get_post_field( 'post_title', get_queried_object_id() ); ?></h1>
+		<h1 class="page-title blog-title"><?php echo get_post_field( 'post_title', get_queried_object_id() ); ?>cha cha cha</h1>
 
-		<div class="loop-description">
+		<div class="loop-description blog-description">
 			<?php echo apply_filters( 'the_content', get_post_field( 'post_content', get_queried_object_id() ) ); ?>
 		</div><!-- .loop-description -->
 
 	<?php } elseif ( is_category() ) { ?>
 
-		<h1 class="loop-title"><?php single_cat_title(); ?></h1>
+		<h1 class="page-title category-title"><?php single_cat_title(); ?></h1>
 
-		<div class="loop-description">
+		<div class="loop-description category-description">
 			<?php echo category_description(); ?>
 		</div><!-- .loop-description -->
 
 	<?php } elseif ( is_tag() ) { ?>
 
-		<h1 class="loop-title"><?php single_tag_title(); ?></h1>
+		<h1 class="page-title tag-title"><?php single_tag_title(); ?></h1>
 
-		<div class="loop-description">
+		<div class="loop-description tag-description">
 			<?php echo tag_description(); ?>
 		</div><!-- .loop-description -->
 
 	<?php } elseif ( is_tax() ) { ?>
 
-		<h1 class="loop-title"><?php single_term_title(); ?></h1>
+		<h1 class="page-title tax-title"><?php single_term_title(); ?></h1>
 
-		<div class="loop-description">
+		<div class="loop-description tax-description">
 			<?php echo term_description( '', get_query_var( 'taxonomy' ) ); ?>
 		</div><!-- .loop-description -->
 
 	<?php } elseif ( is_author() ) { ?>
 
-		<h1 class="loop-title fn n"><?php the_author_meta( 'display_name', get_query_var( 'author' ) ); ?></h1>
+		<h1 class="page-title fn n author-title"><?php the_author_meta( 'display_name', get_query_var( 'author' ) ); ?></h1>
 
-		<div class="loop-description">
+		<div class="loop-description author-description">
 			<?php echo wpautop( get_the_author_meta( 'description', get_query_var( 'author' ) ) ); ?>
 		</div><!-- .loop-description -->
 
 	<?php } elseif ( is_search() ) { ?>
 
-		<h1 class="loop-title"><?php echo esc_attr( get_search_query() ); ?></h1>
+		<h1 class="page-title search-title"><?php echo esc_attr( get_search_query() ); ?></h1>
 
-		<div class="loop-description">
+		<div class="loop-description search-description">
 			<?php echo wpautop( sprintf( __( 'You are browsing the search results for "%s"', 'hybrid-base' ), esc_attr( get_search_query() ) ) ); ?>
 		</div><!-- .loop-description -->
 
@@ -58,9 +58,9 @@ if ( is_singular() )
 
 		<?php $post_type = get_post_type_object( get_query_var( 'post_type' ) ); ?>
 
-		<h1 class="loop-title"><?php post_type_archive_title(); ?></h1>
+		<h1 class="page-title cpt-title"><?php post_type_archive_title(); ?></h1>
 
-		<div class="loop-description">
+		<div class="loop-description cpt-description">
 			<?php if ( !empty( $post_type->description ) ) echo wpautop( $post_type->description ); ?>
 		</div><!-- .loop-description -->
 
@@ -75,17 +75,17 @@ if ( is_singular() )
 				$date = get_the_time( __( 'Y', 'hybrid-base' ) );
 		?>
 
-		<h1 class="loop-title"><?php echo $date; ?></h1>
+		<h1 class="page-title date-title"><?php echo $date; ?></h1>
 
-		<div class="loop-description">
+		<div class="loop-description date-description">
 			<?php echo wpautop( sprintf( __( 'You are browsing the site archives for %s.', 'hybrid-base' ), $date ) ); ?>
 		</div><!-- .loop-description -->
 
 	<?php } elseif ( is_archive() ) { ?>
 
-		<h1 class="loop-title"><?php _e( 'Archives', 'hybrid-base' ); ?></h1>
+		<h1 class="page-title archive-title"><?php _e( 'Archives', 'hybrid-base' ); ?></h1>
 
-		<div class="loop-description">
+		<div class="loop-description archive-description">
 			<?php echo wpautop( __( 'You are browsing the site archives.', 'hybrid-base' ) ); ?>
 		</div><!-- .loop-description -->
 
