3c6016d624ef43e0133f56fc89ab8350c8d0da8e
Bryan Hoffman <bryan@spigotdesign.com>
2013-03-21 16:59:10 -0600

    Moved theme setup function to child theme; Version bump -> 1.2.2

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  *
  * @package infusion
  * @subpackage Functions
- * @version 1.2.1
+ * @version 1.2.2
  * @author Bryan Hoffman
  * @copyright Copyright (c) 2013 Bryan Hoffman
  * @link http://spigotdesign.com/
@@ -17,28 +17,4 @@
 require_once( trailingslashit( TEMPLATEPATH ) . 'library/hybrid.php' );
 $theme = new Hybrid();
 
-/* Do theme setup on the 'after_setup_theme' hook. */
-add_action( 'after_setup_theme', 'infusion_theme_setup' );
-
-/**
- * Theme setup function.  This function adds support for theme features and defines the default theme
- * actions and filters.
- *
- * @since 1.0
- */
-function infusion_theme_setup() {
-
-	/* Get action/filter hook prefix. */
-	$prefix = hybrid_get_prefix();
-
-	/* Add theme support for core framework features. */
-	add_theme_support( 'hybrid-core-widgets' );
-	add_theme_support( 'hybrid-core-shortcodes' );
-	add_theme_support( 'hybrid-core-theme-settings', array( 'about', 'footer' ) );
-	add_theme_support( 'hybrid-core-template-hierarchy' );
-	
-	/* Add support for auto-feed links. */
-	add_theme_support( 'automatic-feed-links' );
-	
-}
 ?>
