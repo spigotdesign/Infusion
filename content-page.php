58a2096a5ee462e5468ebb60575df1fe934862e7
Bryan Hoffman <bryan@spigotdesign.com>
2013-10-14 10:11:40 -0600

    Updated post attributes to hybrid_post_attributes function
    Signed-off-by: Bryan Hoffman <bryan@spigotdesign.com>

--- content-page.php
+++ content-page.php
@@ -1,8 +1,8 @@
-<article id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">
+<article <?php hybrid_post_attributes(); ?>>
 
 	<?php if ( is_singular( get_post_type() ) ) { ?>
 
-		<?php the_title( '<h1 class="page-title"><a href="' . get_permalink() . '">', '</a></h1>' ); ?>
+		<h1 class="entry-title page-title"><?php single_post_title(); ?></h1>
 
 		<div class="page-content">
 			<?php the_content(); ?>
@@ -11,10 +11,9 @@
 
 	<?php } else { ?>
 
-		<?php the_title( '<h2 class="page-title"><a href="' . get_permalink() . '">', '</a></h2>' ); ?>
+		<?php the_title( '<h2 class="entry-title page-title"><a href="' . get_permalink() . '">', '</a></h2>' ); ?>
 
 		<div class="page-summary">
-			<?php if ( current_theme_supports( 'get-the-image' ) ) get_the_image(); ?>
 			<?php the_excerpt(); ?>
 			<?php wp_link_pages( array( 'before' => '<p class="page-links">' . '<span class="before">' . __( 'Pages:', 'infusion' ) . '</span>', 'after' => '</p>' ) ); ?>
 		</div>
