ae826fab9639b1bb2ef42d11a3706bd685b1f6c4
Bryan Hoffman <bryan@spigotdesign.com>
2014-09-17 14:57:59 -0600

    Comments and Content template cleanup
    Signed-off-by: Bryan Hoffman <bryan@spigotdesign.com>

--- content/content.php
+++ content/content.php
@@ -11,19 +11,19 @@
 				<time <?php hybrid_attr( 'entry-published' ); ?>><?php echo get_the_date(); ?></time>
 				<?php comments_popup_link( number_format_i18n( 0 ), number_format_i18n( 1 ), '%', 'comments-link', '' ); ?>
 				<?php edit_post_link(); ?>
-			</div><!-- .entry-byline -->
+			</div>
 
-		</header><!-- .entry-header -->
+		</header>
 
 		<div <?php hybrid_attr( 'entry-content' ); ?>>
 			<?php the_content(); ?>
 			<?php wp_link_pages(); ?>
-		</div><!-- .entry-content -->
+		</div>
 
 		<footer class="entry-footer">
 			<?php hybrid_post_terms( array( 'taxonomy' => 'category', 'text' => __( 'Posted in %s', 'infusion' ) ) ); ?>
 			<?php hybrid_post_terms( array( 'taxonomy' => 'post_tag', 'text' => __( 'Tagged %s', 'infusion' ), 'before' => '<br />' ) ); ?>
-		</footer><!-- .entry-footer -->
+		</footer>
 
 	<?php else : // If not viewing a single post. ?>
 
@@ -38,14 +38,19 @@
 				<time <?php hybrid_attr( 'entry-published' ); ?>><?php echo get_the_date(); ?></time>
 				<?php comments_popup_link( number_format_i18n( 0 ), number_format_i18n( 1 ), '%', 'comments-link', '' ); ?>
 				<?php edit_post_link(); ?>
-			</div><!-- .entry-byline -->
+			</div>
 
 		</header><!-- .entry-header -->
 
 		<div <?php hybrid_attr( 'entry-summary' ); ?>>
 			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
+		</div>
+
+		<footer class="entry-footer">
+			<?php hybrid_post_terms( array( 'taxonomy' => 'category', 'text' => __( 'Posted in %s', 'infusion' ) ) ); ?>
+			<?php hybrid_post_terms( array( 'taxonomy' => 'post_tag', 'text' => __( 'Tagged %s', 'infusion' ), 'before' => '<br />' ) ); ?>
+		</footer>
 
 	<?php endif; // End single post check. ?>
 
-</article><!-- .entry -->
+</article>
--- content/page.php
+++ content/page.php
@@ -2,26 +2,18 @@
 
 	<?php if ( is_page() ) : // If viewing a single page. ?>
 
-		<header class="entry-header">
-			<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php single_post_title(); ?></h1>
-		</header><!-- .entry-header -->
+		<h1 <?php hybrid_attr( 'entry-title' ); ?>><?php single_post_title(); ?></h1>
 
 		<div <?php hybrid_attr( 'entry-content' ); ?>>
 			<?php the_content(); ?>
 			<?php wp_link_pages(); ?>
 		</div><!-- .entry-content -->
 
-		<footer class="entry-footer">
-			<?php edit_post_link(); ?>
-		</footer><!-- .entry-footer -->
-
 	<?php else : // If not viewing a single page. ?>
 
 		<?php get_the_image( array( 'size' => 'full' ) ); ?>
-
-		<header class="entry-header">
-			<?php the_title( '<h2 ' . hybrid_get_attr( 'entry-title' ) . '><a href="' . get_permalink() . '" rel="bookmark" itemprop="url">', '</a></h2>' ); ?>
-		</header><!-- .entry-header -->
+		
+		<?php the_title( '<h2 ' . hybrid_get_attr( 'entry-title' ) . '><a href="' . get_permalink() . '" rel="bookmark" itemprop="url">', '</a></h2>' ); ?>
 
 		<div <?php hybrid_attr( 'entry-summary' ); ?>>
 			<?php the_excerpt(); ?>
