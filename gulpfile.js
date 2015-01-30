aa5d8072eb8d84dab8eb46793633f33527d4a8ff
Bryan Hoffman <bryan@spigotdesign.com>
2015-01-30 12:08:50 -0700

    gulp-ruby-sass updated
    Damn that took too long...
    
    Signed-off-by: Bryan Hoffman <bryan@spigotdesign.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,30 @@
 var gulp      = require('gulp'),
   sass        = require('gulp-ruby-sass'),
+  sourcemaps  = require('gulp-sourcemaps'),
   livereload  = require('gulp-livereload'),
-  plumber     = require('gulp-plumber'),
   prefix      = require('gulp-autoprefixer'),
+  plumber     = require('gulp-plumber'),
   notify      = require("gulp-notify") ,
   uglify      = require('gulp-uglify'),
-  newer       = require('gulp-newer'),
   rename      = require("gulp-rename"),
-  imagemin    = require('gulp-imagemin');
-  pngquant    = require('imagemin-pngquant');;
+  imagemin    = require('gulp-imagemin'),
+  pngquant    = require('imagemin-pngquant'),
+  concat      = require('gulp-concat'),
+  stripDebug  = require('gulp-strip-debug');
 
 
 // Styles
 gulp.task('styles', function() {
-  gulp.src('scss/**/*.scss')
-  .pipe(plumber())
-  .pipe(sass({sourcemapPath: 'source', style: 'expanded'}))
+
+  return sass('scss/style.scss', { style: 'expanded'})
+  .on('error', function (err) {
+      console.error('Error!', err.message);
+   })
   .pipe(gulp.dest('.'))
   .pipe(livereload())
-  .pipe(notify({ message: 'Styles task complete' }));
 });
 
-// Scripts
+// Compress JS
 gulp.task('compress', function() {
   gulp.src('js/*.js')
 
@@ -31,27 +34,42 @@ gulp.task('compress', function() {
     .pipe(notify({ message: 'Compression task complete' }));
 });
 
+// Concat Plugins, Minify
+gulp.task('plugins', function() {
+    gulp.src(['js/plugins/*.js'])
+    .pipe(concat('plugins.js'))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min' }))
+    .pipe(gulp.dest('js/build'));
+});
+
 // Watch
 
 gulp.task('watch', function(){
-  var server = livereload();
-
+  livereload.listen();
   gulp.watch('scss/**/*.scss', ['styles']);
   gulp.watch('js/*.js', ['compress']);
-  gulp.watch('**/*.php').on('change', function(file) {
-      livereload(server).changed(file.path);
-  });
+  gulp.watch('js/plugins/*.js', ['plugins']);
+
+
 });
 
 // Images
 gulp.task('images', function () {
-    return gulp.src('img/**/*')
+    return gulp.src('img/icons/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('img/build/'));
+        .pipe(gulp.dest('img/build/'))
+        .pipe(notify({ // Add gulpif here
+           title: 'Gulp',
+           subtitle: 'Success!',
+           message: 'Successfully compressed images',
+           sound: "Beep"
+       }));
 });
 
 // Default
