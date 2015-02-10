7d3e641c7b74d8c24c300209857b8ec18b1c09bc
Bryan Hoffman <bryan@spigotdesign.com>
2015-02-10 11:25:59 -0700

    Added no-js for Modernizr
    Signed-off-by: Bryan Hoffman <bryan@spigotdesign.com>

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributes( 'html' ); ?>>
+<html <?php language_attributes( 'html' ); ?> class="no-js">
 
 <head>
 	<?php wp_head(); // Hook required for scripts, styles, and other <head> items. ?>
