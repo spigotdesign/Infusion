05a4466913a6ccb8ae43108ce913b179808fda75
Bryan Hoffman <bryan@spigotdesign.com>
2016-05-20 12:08:44 -0600

    index file cleanup
    Removed potentially superflourious divs
    
    Signed-off-by: Bryan Hoffman <bryan@spigotdesign.com>

--- index.php
+++ index.php
@@ -1,41 +1,33 @@
 <?php get_header(); ?>
 
-	<div class="main">
+	<?php if ( !is_front_page() && !is_singular() && !is_404() ) : ?>
 
-		<?php if ( !is_front_page() && !is_singular() && !is_404() ) : ?>
+		<?php locate_template( array( 'inc/loop-meta.php' ), true ); ?>
 
-			<?php locate_template( array( 'inc/loop-meta.php' ), true ); ?>
+	<?php endif; ?>
 
-		<?php endif; ?>
+	<?php if ( have_posts() ) : ?>
 
-		<div class="page-contents">
+	<?php while ( have_posts() ) :  ?>
 
-		<?php if ( have_posts() ) : ?>
+		<?php the_post();  ?>
 
-		<?php while ( have_posts() ) :  ?>
+		<?php hybrid_get_content_template(); ?>
 
-			<?php the_post();  ?>
+		<?php if ( is_singular() && !is_page() ) : ?>
 
-			<?php hybrid_get_content_template(); ?>
-
-			<?php if ( is_singular() && !is_page() ) : ?>
-
-				<?php comments_template( '', true ); ?>
-
-			<?php endif; ?>
-
-		<?php endwhile; ?>
-
-		</div>
-
-		<?php locate_template( array( 'inc/loop-nav.php' ), true ); ?>
-
-		<?php else : ?>
-
-			<?php locate_template( array( 'content/error.php' ), true ); ?>
+			<?php comments_template( '', true ); ?>
 
 		<?php endif; ?>
 
-	</div>
+	<?php endwhile; ?>
+
+	<?php locate_template( array( 'inc/loop-nav.php' ), true ); ?>
+
+	<?php else : ?>
+
+		<?php locate_template( array( 'content/error.php' ), true ); ?>
+
+	<?php endif; ?>
 
 <?php get_footer();  ?>
